8119ef6bcd6c4815c57ff1747a9c2f3d6107a689
Sviatoslav Kalinichuk <svyatoslav.pl@gmail.com>
2025-12-26 19:49:31 +0100

    deploy fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
 /** @type {import('next').NextConfig} */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   turbopack: {
     root: __dirname,
